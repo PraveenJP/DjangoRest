69b6bcb68c25eecc71b8c5721b4e08199b8c8fff
praveenjp <roadizpraveen@gmail.com>
2017-08-25 16:05:24 +0530

    Swagger UI name changed

--- Backend/urls.py
+++ Backend/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='Sample API')
 
 router = DefaultRouter()
 router.register(r'student',StudentControl,base_name='student')
