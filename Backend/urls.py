6b5ec89628f5f18d4664b2ad25fbecab05406108
praveenjp <roadizpraveen@gmail.com>
2017-08-25 16:03:43 +0530

    Swagger UI Added

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from .views import *
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 router = DefaultRouter()
 router.register(r'student',StudentControl,base_name='student')
 
 urlpatterns=[
-	url(r'^',include(router.urls),name='index')
+	url(r'^',include(router.urls),name='index'),
+	url(r'^swagger/$', schema_view)
 ]
